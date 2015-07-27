dcbfc6f91182911048a2d647ca4ea3cd4b58a818
KlutchSC <jdandrews108@gmail.com>
2015-07-27 10:15:15 -0700

    First Commit
    First commit of the game Flip

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+    public static GameController controller;
+
+    public bool hasKey;
+
+    public int nextLevel;
+    public int curLevel;
+
+    void Awake()
+    {
+        if (controller == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            controller = this;
+        }
+        else if (controller != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+	void Start()
+    {
+        curLevel = Application.loadedLevel;
+	}
+
+	void Update ()
+    {
+        Debug.Log(Application.loadedLevel);
+	}
+
+    public void FlipGravity()
+    {
+        Physics2D.gravity = new Vector3(0.0f, (Physics2D.gravity.y * -1.0f), 0.0f);
+    }
+
+    public void LoadNewLevel()
+    {
+        if (hasKey == true)
+        {
+            Application.LoadLevel(nextLevel);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    Rigidbody2D myBody;
+    const float moveSpeed = 6.0f;
+
+	void Start()
+    {
+        myBody = this.GetComponent<Rigidbody2D>();
+	}
+	
+	void Update()
+    {
+        CheckInput();
+	}
+
+    void MovePlayer(float direction)
+    {
+        myBody.velocity = new Vector2(((moveSpeed * 1.2f) * direction), myBody.velocity.y);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "LevelExit")
+        {
+            if (GameController.controller.hasKey == true)
+            {
+                GameController.controller.LoadNewLevel();
+            }
+        }
+    }
+
+    void CheckInput()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            MovePlayer(1.0f);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            MovePlayer(-1.0f);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameController.controller.FlipGravity();
+        }
+        if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.A))
+        {
+            MovePlayer(0.0f);
+        }
+    }
+}
