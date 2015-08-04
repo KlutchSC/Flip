f33b716a4b99ece7c12aad5da2a161eed66c8512
KlutchSC <jdandrews108@gmail.com>
2015-08-03 17:03:41 -0700

    Level Revamp
    changed the way levels are constructed using circles instead of blocks

---
+++ Assets/Scripts/FloorScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloorScript : MonoBehaviour {
+
+    SpriteRenderer myRender;
+
+    public Color32 color_01 = new Color32();
+    public Color32 color_02 = new Color32();
+    public Color32 color_03 = new Color32();
+    public Color32 color_04 = new Color32();
+    public Color32 color_05 = new Color32();
+
+	void Start ()
+    {
+        SpriteRenderer sr = gameObject.GetComponent<SpriteRenderer>();
+        sr.color = color_01;
+	}
+	
+	void Update () 
+    {
+	
+	}
+}
